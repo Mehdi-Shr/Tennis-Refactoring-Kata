02943f71f919e52f965da2b3b5f81b8cfa064d08
Mehdi <mehdi.shr@outlook.fr>
2024-04-24 17:44:40 +0200

    formalisme

--- python/tennis6.py
+++ python/tennis6.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
 
 class TennisGame6:
+    # 1) Les noms des joueurs devraient être des chaînes de caractères
+    # Problème : Les noms des joueurs peuvent être de tout type, ce qui peut causer des erreurs imprévues.
+    # Recommandation : Vérifier que les noms des joueurs sont des chaînes de caractères non vides avant de les utiliser. typé les noms des joueurs
+    def __init__(self, player1Name, player2Name):
     def __init__(self, player1Name, player2Name):
         self.player1Name = player1Name
         self.player2Name = player2Name
         self.player1Score = 0
         self.player2Score = 0
 
+    # 2) Simplification de la condition (amélioration de la lisibilité)
+    # Problème : La condition `playerName == "player1"` est redondante et peut être simplifiée.
+    # Recommandation : Remplacer la condition par `if playerName == self.player1Name`.
     def won_point(self, playerName):
         if (playerName == "player1"):
             self.player1Score += 1
         else:
             self.player2Score += 1
 
+
+    # 3) Noms de variables descriptifs
+    # Problème : Les noms de variables `endGameScore` et `regularScore` ne sont pas assez descriptifs.
+    # Recommandation : Utiliser des noms de variables plus explicites, tels que `finalScore` et `regularScore`.
     def score(self):
         result: str
 
@@ -30,6 +41,9 @@ class TennisGame6:
                     tieScore = "Deuce"
 
             result = tieScore
+        # 4) Constante à réaliser et expliqué
+        # Problème : Pourquoi le nombre 4 ?
+        # Recommandation : Creer des constantes.
         elif (self.player1Score >= 4 or self.player2Score >= 4):
             # end-game score
             endGameScore: str
@@ -47,6 +61,10 @@ class TennisGame6:
         else:
             # regular score
             regularScore: str
+
+            # 5) L'usage de match-case n'est pas cohérent avec le reste du code Python
+            # son usage ici peut rendre le code moins compréhensible pour ceux qui ne sont pas familiers avec cette syntaxe.
+            # regular score
 
             match (self.player1Score):
                 case 0:
